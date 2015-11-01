e84162eb610af096beafa61e7de982e6e38b69d7
dubuqingfeng <1135326346@qq.com>
2015-11-01 13:57:30 +0800

    恢复归档功能和更新博客配置

--- blogconfig/pelicanconf.py
+++ blogconfig/pelicanconf.py
@@ -2,27 +2,26 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'admin'
-SITENAME = u'独步清风\'s blog'
-SITEURL = 'http://dbqf.xyz'
+AUTHOR = u'dubuqingfeng'
+SITENAME = u"\u72ec\u6b65\u6e05\u98ce's blog"
+SITEURL = 'dubuqingfeng.github.io'
 
 PATH = 'content'
 
 TIMEZONE = 'Europe/Paris'
 
-DEFAULT_LANG = u'zh'
+DEFAULT_LANG = u'cn'
 
 # Feed generation is usually not desired when developing
 FEED_RSS = u"feeds/all.rss.xml"
 CATEGORY_FEED_RSS=u"feeds/%s.rss.xml"
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Changyanlong', 'http://getpelican.com/'),
+LINKS = (('Pelican', 'http://getpelican.com/'),
          ('1433', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
          ('', '#'),)
@@ -33,14 +32,19 @@ SOCIAL = (('Github', 'http://github.com/dubuqingfeng'),
 
 DEFAULT_PAGINATION = 10
 
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
 # Theme
 THEME = 'pelican-themes/yeti'
 DEFAULT_DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 DISQUS_SITENAME = u"dubuqingfeng"
 GITHUB_USER = u"dubuqingfeng"
-GITHUB_REPO_COUNT= 5
-GITHUB_SKIP_FORK= False
-GITHUB_SHOW_USER_LINK= False
+
+ARTICLE_URL = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/index.html'
+ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/index.html'
+YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/index.html'
 
 # Plugin
 PLUGIN_PATHS = ['pelican-plugins']
@@ -48,18 +52,15 @@ PLUGINS = [ 'sitemap', 'gravatar' ]
 
 # Sitemap
 SITEMAP = {
-  'format': 'xml',
-  'priorities': {
-    'articles': 1,
+    'format': 'xml',
+        'priorities': {
+'articles': 1,
     'pages': 0.9,
     'indexes': 0.8,
-  },
-  'changefreqs': {
-    'indexes': 'daily',
+        },
+            'changefreqs': {
+'indexes': 'daily',
     'articles': 'daily',
     'pages': 'weekly'
-  }
+    }
 }
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
--- blogconfig/publishconf.py
+++ blogconfig/publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'n'
+SITEURL = 'http://dbqf.xyz'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
